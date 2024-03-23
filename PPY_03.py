d0f51ebb44ce22cf1309dcea5115b30d3be09c99
KamTom <kamiltomala04@gmail.com>
2024-03-23 17:38:11 +0100

    Task 2: Functions

--- PPY_03.py
+++ PPY_03.py
@@ -1,6 +1,19 @@
 # task 1
-list1 = [1,2,3,4,5,6,7,8,9,10]
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 squared_list = [i*i for i in list1]
 print(list1)
 print(squared_list)
+
+# task 2
+
+def squared(start, end):
+    res = []
+    for i in range(start, end + 1):
+        res.append(i**2)
+    return res
+
+print("task2: ", squared(1, 10))
+
+
+
