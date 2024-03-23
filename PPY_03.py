4eb170a5111170c1c4f90c0e044cf9f3238bde90
KamTom <kamiltomala04@gmail.com>
2024-03-23 18:34:36 +0100

    Task 5: Exceptions

--- PPY_03.py
+++ PPY_03.py
@@ -5,7 +5,7 @@ list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 squared_list = [i*i for i in list1]
 print(list1)
-print("Task 1", squared_list)
+print("task1: ", squared_list)
 
 # task 2
 
@@ -33,5 +33,21 @@ print("task3: ", my_class.class_squared(1, 10))
 # task 4
 
 library_list = [math.pow(i, 2) for i in list1]
-print(library_list)
+print("task4: ", library_list)
+
+# task 5
+
+class MyException(Exception):
+    pass
+
+
+class SquareGenerator:
+    def sq_squared(self, start, end):
+        if start < end:
+            raise MyException("start should be greater than end")
+        return squared(start, end)
+
+
+squared_generator = SquareGenerator()
+print("task5: ", squared_generator.sq_squared(3, 1))
 
